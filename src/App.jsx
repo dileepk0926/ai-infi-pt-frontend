b60e496faf68cf37e2b2afbf6c774fa515e954d7
dileepk0926 <dileepkumar.bs@infinitisoftware.net>
2025-06-06 19:13:40 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-6 text-center text-xl text-blue-600">
+      <h1>AI INFI PT - Web Version</h1>
+    </div>
+  );
+}
+
+export default App;
